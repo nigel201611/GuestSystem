e529869a54419317ec5aeb501a1bbda674491860
nigel201611 <1149828479@qq.com>
2017-07-15 16:15:06 +0800

    update

---
+++ data/deleteMessage.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nigel
+ * Date: 2017/7/15
+ * Time: 15:29
+ */
+
+require_once "phpClass/MySQL.class.php";
+
+$db = MySQL::getObj('localhost','root','root','nigel');
+
+$messageArr = [
+    'code'=>-1,
+    'msg'=>""
+];
+if(isset( $_SESSION['username']) && !empty( $_SESSION['username'])){
+    $to_user = $_SESSION['username'];
+}else{
+    $messageArr['code'] = 1;//需要重新登录
+    $messageArr['msg'] = "login timeout,please login again";
+    echo json_encode($messageArr);
+    exit();
+}
+
+$ids = $_GET['id'];
+
+$db->delete('message','id = {$ids}');
