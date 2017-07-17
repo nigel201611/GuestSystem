89bb98448d83fc4c72910c132bf282798b0b8550
nigel201611 <1149828479@qq.com>
2017-07-17 20:13:19 +0800

    upate

---
+++ data/getMessageCount.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nigel
+ * Date: 2017/7/17
+ * Time: 19:47
+ */
+
+require_once 'phpClass/MySQL.class.php';
+
+$db = MySQL::getObj('localhost','root','root','nigel');
+
+$username = $_GET['username'];
+
+//获取总记录数
+$config = [
+    'mode'=>MYSQLI_ASSOC,
+    'fileds'=>"id",
+    'where'=>"to_user='{$username}'",  //条件是，别人发给自己的
+];
+$result = $db->fetchAll('message',$config);
+
+if($result){
+    echo count($result);
+}
+
