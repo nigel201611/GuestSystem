2a751d66831812d56fcf84cb03dda788ab454a21
nigel201611 <1149828479@qq.com>
2017-08-03 22:26:22 +0800

    update修改照片名称

---
+++ data/modifyPhoto.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nigel
+ * Date: 2017/7/16
+ * Time: 15:06
+ */
+
+header('content-type:application/json;');
+session_start();
+require_once 'phpClass/MySQL.class.php';
+date_default_timezone_set('prc');
+
+@$albumName = $_POST['albumName'];
+@$albumName = $_POST['origianlPhotoName'];
+$id = $_POST['id'];//相册id
+
+
+
+$modifyRsp = ['code'=>-1,'msg'=>""];
+if(isset($_SESSION['username']) && !empty($_SESSION['username'])){
+    $username = $_SESSION['username'];
+}else{
+    $modifyRsp['code'] = 2;
+    $modifyRsp['msg'] = "login timeout,please login again";
+    echo json_encode($modifyRsp);
+    exit;
+}
+$conn = MySQL::getObj('localhost','root','root','nigel');
+
+//根据id获取相册路径或目录
+$config = [
+    'mode'=>MYSQLI_ASSOC,
+    'fileds'=>'album_dir',
+    'where'=>'id='.$id
+];
+$result = $conn->fetchOne('album',$config);
+$album_dir = $result['album_dir'];
+
+//修改指定目录下的照片名字rename
+
+//创建一个相册目录
+
+if($num == 1){
+    $modifyRsp['code'] = 1;
+    $modifyRsp['msg'] = "modify success";
+}else{
+    $modifyRsp['code'] = -1;
+    $modifyRsp['msg'] = "modify failed";
+}
+
+echo json_encode($modifyRsp);
+
+
+
+
